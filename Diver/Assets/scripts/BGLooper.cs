7988f7800d1989a47d78512921bfb69507e20b3a
Glen Wong <glen@xoocorp.com>
2014-11-01 17:26:27 -0700

    merge fix

--- Diver/Assets/scripts/BGLooper.cs
+++ Diver/Assets/scripts/BGLooper.cs
@@ -3,19 +3,25 @@ using System.Collections;
 
 public class BGLooper : MonoBehaviour {
 
+	public Transform coin;
+
 	int numBGPanels = 6;
 
 	float pipeMax = 0.8430938f;
 	float pipeMin = -0.003243029f;
 
 	void Start() {
-		GameObject[] pipes = GameObject.FindGameObjectsWithTag("Pipe");
-
-		foreach(GameObject pipe in pipes) {
-			Vector3 pos = pipe.transform.position;
-			pos.y = Random.Range(pipeMin, pipeMax);
-			pipe.transform.position = pos;
-		}
+//		GameObject[] pipes = GameObject.FindGameObjectsWithTag("Pipe");
+//
+//		foreach(GameObject pipe in pipes) {
+//			Vector3 pos = pipe.transform.position;
+//			pos.y = Random.Range(pipeMin, pipeMax);
+//			pipe.transform.position = pos;
+//		}
+		// Create some coins
+//		GameObject coin = Instantiate(coin);
+//		coin.transform.position = new Vector3(0, 0, 0);
+//		Debug.Log ("BGLooper start");
 	}
 
 	void OnTriggerEnter2D(Collider2D collider) {
@@ -27,7 +33,7 @@ public class BGLooper : MonoBehaviour {
 				panelCount = 3;
 				break;
 			case "Sand":
-			panelCount = 5;
+			panelCount = 12;
 				break;
 			default:
 				panelCount = numBGPanels;
