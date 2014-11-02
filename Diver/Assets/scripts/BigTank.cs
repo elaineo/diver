33cb9e5f8dced3a57b81fadf68fea5ff807bbf1e
Warren Mar <warren.mar@gmail.com>
2014-11-02 01:19:20 -0700

    add big oxygen tanks

---
+++ Diver/Assets/scripts/BigTank.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BigTank : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float oxygen = OxygenBar.Oxygen();
+		byte oxygenColor = 100;
+		int o = (int)((1-oxygen) * 100);
+		oxygenColor = (byte)o;
+		guiTexture.color =  new Color32(255, 255, 255, oxygenColor);
+		//GUI.color =  new Color32(255, 255, 255, 10);
+	}
+}
