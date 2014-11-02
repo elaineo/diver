d178510e3f8f541d333092be354658db8e40d17a
Warren Mar <warren.mar@gmail.com>
2014-11-02 04:50:26 -0800

    font resize

---
+++ Diver/Assets/scripts/fontResize.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fontResize : MonoBehaviour {
+	const float REFERENCE_SCREEN_WIDTH = 800.0f;
+
+	// Use this for initialization
+	void Start () {
+		float scaleFactor = (Screen.width/REFERENCE_SCREEN_WIDTH);
+		guiText.fontSize = (int)(24*scaleFactor);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
