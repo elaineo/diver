d03436eae910d13e97dfc7acb467b049eb0b310a
elaineo <elaine.ou@gmail.com>
2014-11-01 23:25:09 -0700

    bullet

--- Diver/Assets/scripts/Shooter.cs
+++ Diver/Assets/scripts/Shooter.cs
@@ -66,7 +66,8 @@ public class Shooter : MonoBehaviour
 			for(int i = 0; i < shotPositions.Length; i++)
 			{
 				// create a bullet
-				Instantiate(bullet, shotPositions[i].position, shotPositions[i].rotation);
+				//bullet.transform.rotation = Quaternion.Euler(0, 0, 90.0f);
+				Instantiate(bullet, shotPositions[i].position, shotPositions[i].rotation  );
 				//Activate it
 				//obj.SetActive(true);
 				//Debug.Log(obj);
