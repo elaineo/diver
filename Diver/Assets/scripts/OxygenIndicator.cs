b8317149a377e0eec30a21826f3b240e38ea0ef8
Warren Mar <warren.mar@gmail.com>
2014-11-02 01:05:40 -0800

    oxygen bars work

---
+++ Diver/Assets/scripts/OxygenIndicator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OxygenIndicator : MonoBehaviour {
+	public float level = 1.0f;
+	float warning_level = 0.3f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate () {
+		float oxygen = OxygenBar.Oxygen();
+		if (oxygen < warning_level) {
+			guiTexture.texture = Resources.Load ("oxygenbar_red") as Texture;
+		}
+		if (oxygen <= level) {
+			guiTexture.texture = Resources.Load ("oxygenbar_empty") as Texture;
+		}
+	}
+}
