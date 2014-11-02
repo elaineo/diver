67c65edf2d2af92fd10b1148d92351d49823ba15
Glen Wong <glen@xoocorp.com>
2014-11-02 01:07:39 -0700

    title screen

---
+++ Diver/Assets/scripts/TitleScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0))) {
+			Application.LoadLevel("scene");
+		}
+	}
+}
