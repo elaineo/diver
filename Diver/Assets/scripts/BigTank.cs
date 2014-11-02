02f3e1a559eba5cee03ef49bab03818d15753c21
Warren Mar <warren.mar@gmail.com>
2014-11-02 01:25:45 -0800

    animate glow

--- Diver/Assets/scripts/BigTank.cs
+++ Diver/Assets/scripts/BigTank.cs
@@ -2,19 +2,23 @@
 using System.Collections;
 
 public class BigTank : MonoBehaviour {
-
+	float time = 0;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		float oxygen = OxygenBar.Oxygen();
-		byte oxygenColor = 100;
-		int o = (int)((1-oxygen) * 100);
-		oxygenColor = (byte)o;
-		guiTexture.color =  new Color32(255, 255, 255, oxygenColor);
-		//GUI.color =  new Color32(255, 255, 255, 10);
+		if (oxygen < 0.3f) {
+			time += Time.deltaTime;
+			oxygen = Mathf.Sin (time*5.0f)*0.5f + 0.5f;
+			byte oxygenColor = 100;
+			int o = (int)((1-oxygen) * 100);
+			oxygenColor = (byte)o;
+			guiTexture.color =  new Color32(255, 255, 255, oxygenColor);
+			//GUI.color =  new Color32(255, 255, 255, 10);
+		}
 	}
 }
