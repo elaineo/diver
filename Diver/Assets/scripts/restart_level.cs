b7541cd47e2aac1daeb667266ea0d97a98b9213a
Warren Mar <warren.mar@gmail.com>
2014-11-02 13:47:06 -0800

    otter play button

--- Diver/Assets/scripts/restart_level.cs
+++ Diver/Assets/scripts/restart_level.cs
@@ -10,6 +10,13 @@ public class restart_level : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		int count = Input.touchCount;
+		for (int i = 0; i < count; i++) {
+			Touch touch = Input.GetTouch (i);
+			if (guiTexture.HitTest (touch.position)) {
+				Debug.Log ("hit");
+			}
+		}
+
 	}
 }
