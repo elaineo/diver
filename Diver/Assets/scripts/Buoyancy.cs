b50db5d316b1cea8f64cff6872790759e5fcc92b
Glen Wong <glen@xoocorp.com>
2014-11-02 01:52:55 -0800

    title updates

---
+++ Diver/Assets/scripts/Buoyancy.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buoyancy : MonoBehaviour {
+	public float UpwardForce = 120.0f;
+	//public GameObject SplashParticles;
+	private bool isInWater = false;
+	private bool splash = false;
+	private BoxCollider2D water;
+	void OnTriggerEnter2D(Collider2D Other){
+		if (Other.tag == "Water") {
+			isInWater = true;
+			rigidbody2D.drag = 2f;
+			rigidbody2D.angularDrag = 2f;
+			water = (BoxCollider2D)Other;
+		}
+//		if (Other.tag == "Water" && !splash){
+//			Instantiate(SplashParticles, transform.position, Quaternion.identity);
+//			splash = true;
+//		}
+		
+	}
+	
+	void OnTriggerExit2D(Collider2D Other){
+		if (Other.tag == "Water"){
+			isInWater = false;
+			rigidbody2D.drag = 0.5f;
+			rigidbody2D.angularDrag = 0.5f;
+			water = null;
+		}
+	}
+	
+	void FixedUpdate(){
+		if (isInWater){
+			Vector2 force = Vector2.up * UpwardForce;
+			BoxCollider2D col = (BoxCollider2D)collider2D;
+			float intersectionY = Mathf.Min(Mathf.Max(water.bounds.max.y - col.bounds.min.y, 0), col.bounds.size.y);
+			Debug.Log ("Intersection: " + intersectionY.ToString());
+			float percentage = (intersectionY / col.size.y);
+			this.rigidbody2D.AddForce(force * percentage * rigidbody2D.mass, ForceMode2D.Force);
+			Debug.Log(isInWater);
+		}
+	}
+}
--- Diver/Assets/scripts/TitleScript.cs
+++ Diver/Assets/scripts/TitleScript.cs
@@ -11,7 +11,18 @@ public class TitleScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if ((Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0))) {
-			Application.LoadLevel("scene");
+			StartCoroutine(Transition());
+			//Application.LoadLevel("scene");
 		}
+	}
+
+	IEnumerator Transition() {
+		Debug.Log ("Transition");
+		for (float i = 0.0f; i < 1.0f; i+= 0.01f) {
+			transform.Translate(new Vector3(0.0f, 0.1f, 0.0f));			
+			transform.localScale += new Vector3(0.0f, 0.1f, 0.0f);
+			yield return null;
+		}
+		Application.LoadLevel("scene");
 	}
 }
