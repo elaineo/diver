7d74d874a98b0752dc671e993b5e76bfef2a848a
elaineo <elaine.ou@gmail.com>
2014-11-01 17:39:21 -0700

    modify spawner script

--- Diver/Assets/scripts/Spawner.cs
+++ Diver/Assets/scripts/Spawner.cs
@@ -7,14 +7,35 @@ public class Spawner : MonoBehaviour
 	public float spawnDelay = 3f;		// The amount of time before spawning starts.
 	public GameObject[] enemies;		// Array of enemy prefabs.
 
+	Transform player;
+
+	float offsetX;
 
 	void Start ()
 	{
 		// Start calling the Spawn function repeatedly after a delay .
 		InvokeRepeating("Spawn", spawnDelay, spawnTime);
+
+		GameObject player_go = GameObject.FindGameObjectWithTag("Player");
+
+		if(player_go == null) {
+			Debug.LogError("Couldn't find an object with tag 'Player'!");
+			return;
+		}
+
+		player = player_go.transform;
+
+		offsetX = transform.position.x - player.position.x;
+
 	}
 
-
+	void Update () {
+		if(player != null) {
+			Vector3 pos = transform.position;
+			pos.x = player.position.x + offsetX;
+			transform.position = pos;
+		}
+	}
 	void Spawn ()
 	{
 		// Instantiate a random enemy.
